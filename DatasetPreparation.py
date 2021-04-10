d2b4e4fc29f77f7d4674f92d2215de876f17957e
Pitzs <72802494+Pitzs@users.noreply.github.com>
2021-04-10 20:58:53 +0200

    Implemented extraction of TEE

--- DatasetPreparation.py
+++ DatasetPreparation.py
@@ -13,6 +13,7 @@ from pathlib import Path
 
 
 # todo: We can add function to extract also the TEE images from the hdf5 format
+# todo: Remain the extraction of the ground truth
 def prepare_image_data_gt(directory_train, patient, type):
     """
     This function is used to extract ES ED pair of images data and it's ground truth image's labels, from the CAMUS
@@ -38,7 +39,7 @@ def save_medimage_to_PIL(image, main_directory, patient, image_type, type='gray'
     """
     Function used to convert from medimage formato to a tif format used for the training, before there will be a padding
     in order to have a square image and a resize
-    :param image: medimage format
+    :param image: medimage format (TTE Image)
     :param main_directory: Directory where to save the image
     :param patient: patient string
     :param image_type: choose the image_type (['2CH_ED', '2CH_ES','4CH_ED','4CH_ES'])
@@ -77,7 +78,7 @@ def save_medimage_to_PIL(image, main_directory, patient, image_type, type='gray'
 def equal_pixel(image_data, image_gt, pixel_spacing=None):
     """
     Function used to resample the image in order to have a isotropic image
-    :param image_data: Medimage data that have a gray data
+    :param image_data: Medimage data that have a gray data (TTE image)
     :param image_gt: Ground truth image
     :param pixel_spacing: (float) Define a costum pixel_spacing in the image in order to have iso-tropic image
     :return: prepro_image_data: Resampled image with the new pixel_spacing
@@ -106,24 +107,47 @@ def main():
     type_tte = ['2CH_ED', '2CH_ES', '4CH_ED', '4CH_ES']
 
     # Directory where i would like to store the images extracted from the CAMUS
-    saving_directory = Path('extracted_CAMUS')
+    saving_directory_tte = Path('extracted_CAMUS')
+    saving_directory_tee = Path('extracted_TEE')
 
     flag_equal_pixel = False # Leave it as FALSE cause wrong function
     # Remove all the Warning after developing
     warnings.filterwarnings("ignore")
 
-    # Loop through all the patients (EXTRACTION OF THE TEE)
+    # ################## Loop through all the patients (EXTRACTION OF THE TEE) ##################
     for patient in os.listdir(directory_tee):
 
         # Looping through all the hdf5 file for each patient
         for current_hdf5 in os.listdir(os.path.join(directory_tee, patient)):
             path_hdf5 = os.path.join(directory_tee, patient, current_hdf5)
             image_hdf5 = h5py.File(path_hdf5, 'r')
+            # Extract the tissue
+            tissue_hdf5 = image_hdf5['tissue']
+            tissue_data = tissue_hdf5['data']
 
-    # You could generate 15 gb of images
-    #raise Exception('DO NOT RUN THE CODE cause resize function not implemented yet')
+            # Extract the ECG
+            ecg_hdf5 = image_hdf5['ecg']
+            ecg_data = ecg_hdf5['ecg_data']
+            # too many
+            # for idx in range(tissue_hdf5['data'].shape[2]):
+            idx = 0
+            # TEE image
+            PIL_image = Image.fromarray(np.uint8(tissue_data[:, :, idx].squeeze())).convert('RGB')
+            # Padding follows the bigger dimension
+            max_dimension = max(PIL_image.size)
 
-    # Loop through all the patients (EXTRACTION OF THE TTE)
+            # Padding
+            delta_w = max_dimension - PIL_image.size[0]
+            delta_h = max_dimension - PIL_image.size[1]
+            padding = (delta_w // 2, delta_h // 2, delta_w - (delta_w // 2), delta_h - (delta_h // 2))
+            PIL_image = ImageOps.expand(PIL_image, padding)
+
+            # Saving image (gray image)
+            saving_path = os.path.join(saving_directory_tee, 'train_gray', 'gray_' + current_hdf5[:-3] + '_' + str(idx) + '.tif')
+            PIL_image.save(saving_path)
+
+
+    # ################## Loop through all the patients (EXTRACTION OF THE TTE) ##################
     for patient in os.listdir(directory_tte):
 
         # Looping through ['2CH_ED', '2CH_ES', '4CH_ED', '4CH_ES']
@@ -135,9 +159,9 @@ def main():
                 image_data, image_gt = equal_pixel(image_data, image_gt, pixel_spacing=None)
 
             # Saving Images (GRAY DATA)
-            save_medimage_to_PIL(image_data, saving_directory, patient, current_tte, type='gray')
+            save_medimage_to_PIL(image_data, saving_directory_tte, patient, current_tte, type='gray')
             # Saving Images (GROUND TRUTH DATA)
-            save_medimage_to_PIL(image_gt, saving_directory, patient, current_tte, type='gt')
+            save_medimage_to_PIL(image_gt, saving_directory_tte, patient, current_tte, type='gt')
 
 if __name__ == "__main__":
     main()
