a2ba4498d8979a29d4c3f667df6f70cf7664d1c2
Domonkos Balint <domonkob@stud.ntnu.no>
2021-04-20 14:20:52 +0200

    Correct the test function and correct the dice score
    We changed the dice function so now it calculates the f1 score of every sample from the batch, then calculates the average of them.

--- src/test.py
+++ src/test.py
@@ -1,5 +1,5 @@
 from src.utils import to_cuda
-from src.metrics import acc_metric, dice_metric, dice, classwise_f1
+from src.metrics import acc_metric, dice_metric
 from src.visualize import show_segmentation, predb_to_mask
 
 import torch
@@ -9,37 +9,40 @@ from params import *
 # todo: to finish
 
 def test(model, data, visualize=False):
-
-    # Predict on the (validation) data
-    xb, yb = next(iter(data))
-    with torch.no_grad():
-        predb = model(to_cuda(xb))
-
-    if visualize:
-        show_segmentation(xb, yb, predb)
-        
-    # Evaluation - Accuracy
-    accuracy = acc_metric(predb, yb).item()
-    baseline_accuracy = 0.95 
-    print(f"\nFinal Accuracy: {round(accuracy, 4)} (delta to baseline {round(accuracy - baseline_accuracy, 4)})")
-
-    # Evaluation - Dice coefficient
-    baseline_dice =  100
+    # Predict on the validation data
     
-    # IMPLEMENTATION 1: OLD  
-    average_dice, class_dice = dice_metric(predb, yb)
+    if visualize:
+        xb, yb = next(iter(data))
+        with torch.no_grad():
+            predb = model(to_cuda(xb))
+    
+        show_segmentation(xb, yb, predb)
+    
+    accuracy = 0
+    average_dice = 0
+    class_dice = torch.zeros([4])
+    for xb, yb in data:
+        with torch.no_grad():
+            predb = model(to_cuda(xb))
+        
+        accuracy += acc_metric(predb, yb).item()
+        temp_av_dice, temp_class_dice = dice_metric(predb, yb)
+        average_dice += temp_av_dice
+        class_dice += temp_class_dice
+    
+    accuracy /= len(data)
+    average_dice /= len(data)
+    class_dice /= len(data)
+    
+    average_dice = average_dice.numpy()[()]
+    class_dice = class_dice.numpy().tolist()
+    
+    # Evaluation - Accuracy
+    baseline_accuracy = 0.9705810547 # TRAINING TIME: 102m 6s
+    print(f"\nFinal Accuracy: {round(accuracy, 4)} (delta to baseline {(accuracy - baseline_accuracy, 4)})")
 
-    # IMPLEMENTATION 2: Alessandro
-    predb_test = torch.empty(predb.shape[0], predb.shape[2], predb.shape[2])
-    for i in range(predb.shape[0]):
-        predb_test[i, :, :]=predb_to_mask(predb, i)
-    average_dice_A, class_dice_A = dice(predb_test, yb)
-
-    #IMPLEMENTATION 3: Github repo (Dice = f1)
-    average_dice_f, class_dice_f =  classwise_f1(predb, yb)
-   
-    print(f"Final average DICE score (OLD): {round(average_dice, 4)} {class_dice} (delta to baseline {round(average_dice - baseline_dice, 4)})")
-    print(f"Final average DICE score (Ale): {round(average_dice_A, 4)} {class_dice_A} (delta to baseline {round(average_dice_A - baseline_dice, 4)})")
-    print(f"Final average DICE score (GitHub - F1): {round(average_dice_f, 4)} {class_dice_f} (delta to baseline {round(average_dice_f - baseline_dice, 4)})")
-  
-    return accuracy, average_dice_f, class_dice_f
+    # Evaluation - Dice score
+    baseline_dice =  0.607425 # [0.9652, 0.5956, 0.3764, 0.4925]
+    print(f"Final average DICE score: {round(average_dice, 4)} {class_dice} (delta to baseline {round(average_dice - baseline_dice, 4)})")
+    
+    return accuracy, average_dice, class_dice
