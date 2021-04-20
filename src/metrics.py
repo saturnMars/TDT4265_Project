c1e48f317c24f6f91581de86ed22007d1220a05c
Domonkos Balint <domonkob@stud.ntnu.no>
2021-04-20 12:08:10 +0200

    Add domonkos dice implementation

--- src/metrics.py
+++ src/metrics.py
@@ -78,3 +78,26 @@ def classwise_f1(output, gt):
   avg_dice_score = torch.mean(classwise_f1).item()
   class_dice = [round(score.item(), 4) for score in classwise_f1]
   return avg_dice_score, class_dice
+
+def dice_coeff_domonkos(logits, true, eps=1e-7):
+    num_classes = logits.shape[1]
+
+    logits = torch.eye(num_classes)[logits.argmax(1)].permute(0, 3, 1, 2)
+    
+    true_1_hot = torch.eye(num_classes)[true.squeeze(1)]
+    true_1_hot = true_1_hot.permute(0, 3, 1, 2).float()
+
+    true_1_hot = true_1_hot.type(logits.type())
+
+    dims = (2,3)
+    
+    # calculate TP
+    intersection = torch.sum(logits * true_1_hot, dims)
+
+    # calculate 2TP+FN+FP
+    cardinality = torch.sum(logits + true_1_hot, dims)
+
+    dice_loss_class = (2. * intersection / (cardinality + eps)).mean(0)
+    dice_loss_mean = dice_loss_class.mean()
+    
+    return dice_loss_mean, dice_loss_class
