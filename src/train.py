Solve memory leakage bug

src/train.py
@@ -79,19 +79,20 @@ def train(model, train_dl, valid_dl, loss_fn, optimizer, acc_fn, epochs=1):
             # stats - whatever is the phase
             acc = acc_fn(outputs, y)
             dice = dice_metric(outputs, y)
+            
+            with torch.no_grad():
+                running_acc  += acc*train_dl.batch_size
+                running_loss += loss*train_dl.batch_size 
+                if step % 40 == 0:
+                    # clear_output(wait=True)
+                    v_loss, v_acc, v_dice = validation(model, valid_dl, loss_fn, acc_fn)
 
-            running_acc  += acc*train_dl.batch_size
-            running_loss += loss*train_dl.batch_size 
-            if step % 40 == 0:
-                # clear_output(wait=True)
-                v_loss, v_acc, v_dice = validation(model, valid_dl, loss_fn, acc_fn)
+                    valid_loss.append(v_loss)
+                    valid_acc.append(v_acc)
 
-                valid_loss.append(v_loss)
-                valid_acc.append(v_acc)
-                
-                train_loss.append(loss)
-                train_acc.append(acc)
-                print(f'Current step: {step}  Loss: {loss:.4f}  Acc: {acc:.3f} Dice: {dice} Val Loss: {v_loss:.4f} Val Acc: {v_acc:.3f} Val Dice: {v_dice}')
+                    train_loss.append(loss)
+                    train_acc.append(acc)
+                    print(f'Current step: {step}  Loss: {loss:.4f}  Acc: {acc:.3f} Dice: {dice} Val Loss: {v_loss:.4f} Val Acc: {v_acc:.3f} Val Dice: {v_dice}')
 
         epoch_loss = running_loss / len(train_dl.dataset)
         epoch_acc = running_acc / len(train_dl.dataset)